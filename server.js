c5d29a0027b14daf8dc4d31ede98e9481f1ce0cc
matheusaleixo-cit <82680416+matheusaleixo-cit@users.noreply.github.com>
2021-04-16 18:19:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get("/", (req, res)=>{
     res.status(200).send({message: "Hello! This is a test message."})
 })
 
+app.get("/hello", (req, res)=>{
+    res.status(200).send({message: "Hello! This is a test message from the /hello route."})
+})
+
 app.listen(3001, ()=> {
     console.log("Api rodando na porta 3001!");
 })
